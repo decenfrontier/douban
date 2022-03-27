40b8f8ee1746302be88a92992da119b76e7b0c6c
wangshuang <ws156858@163.com>
2022-03-27 23:55:01 +0800

    feat: 底部tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:douban/widgets/dash_line.dart';
+import 'package:douban/pages/home/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,15 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-          body: Center(
-        child: Container(
-          width: 200,
-          child: DashedLine(
-            count: 20,
-          ),
-        ),
-      )),
+      home: MainPage(),
       debugShowCheckedModeBanner: false,
     );
   }
