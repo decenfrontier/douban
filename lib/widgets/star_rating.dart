4f44e240bb97473c5bcd8490461123622527a028
wangshuang <ws156858@163.com>
2022-03-27 20:41:23 +0800

    feat: 根据评分绘制星级

---
+++ lib/widgets/star_rating.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class StarRating extends StatefulWidget {
+  final double rating;
+  final double maxRating;
+  final int count;
+  final double size;  // 每个图形的宽高
+  final Color unselectedColor;
+  final Color selectedColor;
+
+  StarRating({
+    required this.rating,
+    this.maxRating=10,
+    this.count=5,
+    this.size=30,
+    this.unselectedColor=const Color(0xffbbbbbb),
+    this.selectedColor=Colors.orange,
+  });
+
+  @override
+  State<StarRating> createState() => _StarRatingState();
+}
+
+class _StarRatingState extends State<StarRating> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: buildUnselectedStar(),
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: buildSelectedStar(),
+        ),
+      ],
+    );
+  }
+
+  // 构建满填充的star
+  List<Widget> buildSelectedStar() {
+    // star列表
+    List<Widget> stars = [];
+    // 先算出一个star值几分
+    double oneValue = widget.maxRating / widget.count;
+    // 填充整星
+    int entireCount = (widget.rating / oneValue).floor();
+    final star = Icon(Icons.star, color: widget.selectedColor, size: widget.size,);
+    for (var i = 0; i < entireCount; i++) {
+      stars.add(star);
+    }
+    // 填充零星
+    double leftCount = (widget.rating / oneValue) - entireCount;
+    double leftSize = leftCount * widget.size;
+    final part = ClipRect(
+      child: star,
+      clipper: StarClipper(leftSize),
+    );
+    stars.add(part);
+    return stars;
+  }
+
+  List<Widget> buildUnselectedStar() {
+    return List.generate(widget.count, (index) {
+          return Icon(Icons.star_border, color: widget.unselectedColor, size: widget.size);
+        });
+  }
+}
+
+class StarClipper extends CustomClipper<Rect> {
+  double width;
+
+  StarClipper(this.width);
+
+  @override
+  Rect getClip(Size size) {
+    return Rect.fromLTRB(0, 0, width, size.height);
+  }
+
+  @override
+  bool shouldReclip(StarClipper oldClipper) {
+    return oldClipper.width != this.width;
+  }
+}
